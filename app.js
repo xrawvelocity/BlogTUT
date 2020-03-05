12b46dcd4ef3bba1ec2f54d76f9830fbd4dce9b9
Victor Fernandez <vmfs99@gmail.com>
2020-03-04 21:05:22 -0500

    port

--- app.js
+++ app.js
@@ -85,6 +85,11 @@ app.get('/posts/:name', (req,res) => {
 
 
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
